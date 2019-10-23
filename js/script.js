82228ea83010600c9606a3cc8c869fdb994eaac7
wavywave <skybluex32@gmail.com>
2019-10-23 21:59:36 +0000

    add a click handler

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("button").click(function(){
+    var ask = $("input").val();
+    $(".ask")
+    $(".output").append(ask);
 
 
 
-  
-  
-  
-  
+
+
+
 });
